854f0c805a746c4c84eb0fb06d6082a1a0459bb9
Harshit Mendiratta <harshitmendiratta41@gmail.com>
2026-02-18 08:34:38 +0530

    Added JS code folder

---
+++ JS/JavaScript/Code/JS code/script.js
@@ -0,0 +1,36 @@
+//Opeartors
+//Arithmetic,comparison,logical,assignment,unary,ternary
+// == only checks the value but === checks both value and type.
+//assignment operator(=,+=,-=,/=,*=,%=)
+//logical(&&,||,!)
+//unary(applied on single operands(+,-,!,typeof,++,--))
+//(?:) ternary operator -> condition ? true code : false code
+//Q1:
+/*let x=10;
+let y=20;
+if(x>5 && y<25){
+  console.log("A");
+} else{
+  console.log("B");
+}*/
+//q2:
+/*let isAdmin=true;
+let isLoggedin=false;
+if(isAdmin||isLoggedin){
+  console.log("Access granted");
+}else{
+  console.log("Access denied");
+}*/
+/*let temp=35;
+if(!(temp<30)){
+  console.log("Hot");
+} else{
+  console.log("pleasant");
+}*/
+/*let a=0;
+if(a){
+  console.log("Truthy");
+}
+else{
+  console.log("Falsy");
+}*/
