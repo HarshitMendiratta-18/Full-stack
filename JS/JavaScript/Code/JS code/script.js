ccfbe48bcba455f6bcae293cb3632b0f62cfce7a
Harshit Mendiratta <harshitmendiratta41@gmail.com>
2026-02-19 12:27:55 +0530

    Practice js

--- JS/JavaScript/Code/JS code/script.js
+++ JS/JavaScript/Code/JS code/script.js
@@ -34,3 +34,42 @@ if(a){
 else{
   console.log("Falsy");
 }*/
+//control flow
+//if,else,else-if,switch case,early return pattern
+//q1:
+/*function getGrade(score){
+  if(score >=90 && score<=100){
+    return "A+";
+  }
+  else if(score>=80 && score<=89){
+    return "A";
+  }
+  else if(score>=70 && score<=79){
+    return "B";
+  }
+  else if(score>=60 && score<=69){
+    return "C";
+  }
+  else if(score>=33 && score<=59){
+    return "D";
+  }
+  else if(score>=0 && score<=32){
+    return "Fail";
+  }
+  else{
+    return "Invalid Marks";
+  }
+}
+console.log(getGrade(55));*/
+//Rock-paper-scissor logic
+/*function rps(user,computer){
+  if(user===computer) return "draw";
+  if(user==="rock" && computer==="scissor") return "user";
+  if(user==="scissor" && computer==="paper") return "user";
+  if(user==="paper" && computer==="rock") return "user";
+  return "computer";
+
+}
+console.log(rps("rock","scissor"));*/
+//Loops-> repeat karne ko loop kahte hai
+//for while do-while forof forin foreach
