ec28161b23633bb542b102eaa285ff6f38101d85
Harshit Mendiratta <harshitmendiratta41@gmail.com>
2026-02-24 21:41:38 +0530

    learn js

--- JS/JavaScript/Code/JS code/loop.js
+++ JS/JavaScript/Code/JS code/loop.js
@@ -7,7 +7,7 @@
 //1-40 -> for loop
 //20-30 -> for loop
 // hello na aa jay ->while loop
-for(let i=1;i<22;i++){
+/*for(let i=1;i<22;i++){
   console.log(i);
 }
 //while loop
@@ -16,3 +16,66 @@ while(i<10){
   console.log(i);
   i++;
 }
+//do-while loop
+let j=12;
+do{
+  console.log(j);
+  j++;
+}
+while(j<2)
+*/
+
+//q1
+for(let i=1;i<=10;i++){
+  console.log(i);
+}
+//q2
+let i=10;
+while(i>=1){
+  console.log(i);
+  i--;
+}
+//q3
+for(let i=2;i<=20;i+=2){
+  console.log(i);
+}
+//q4
+let j=1;
+while(j<16){
+    console.log(j);
+    j+=2;
+}
+//q5
+for(let i=1;i<11;i++){
+    console.log("5 *",i,"=",(5*i));
+}
+//q6
+let sum=0;
+for(let i=1;i<101;i++){
+    sum = sum+i;
+}
+console.log(sum);
+//q7
+for(let i=2;i<51;i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+//q8
+let num= prompt("Enter the number: ");
+for(i=1;i<=num;i++){
+  if(i%2===0){
+    console.log(`${i} is even.`);
+  }
+  else{
+    console.log(`${i} is odd.`);
+  }
+}
+//q9
+let count=0;
+for(let i=1;i<101;i++){
+    if(i%3===0 && i%5===0){
+        count++;
+    }
+}
+console.log(count);
