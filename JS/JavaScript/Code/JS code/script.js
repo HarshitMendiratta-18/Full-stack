82d8d5f1d5fa4c5fc35a3db7e42d4fd4129c8f17
Harshit Mendiratta <harshitmendiratta41@gmail.com>
2026-05-10 12:28:46 +0530

    type conversion

--- JS/JavaScript/Code/JS code/script.js
+++ JS/JavaScript/Code/JS code/script.js
@@ -72,15 +72,15 @@ console.log(getGrade(55));*/
 }
 console.log(rps("rock","scissor"));*/
 
-console.log("Starting JS");
-document.getElementById("myH1").textContent=`Hello`;
-document.getElementById("myP").textContent=`Welcome my friend!`;
+//console.log("Starting JS");
+//document.getElementById("myH1").textContent=`Hello`;
+//document.getElementById("myP").textContent=`Welcome my friend!`;
 //window.alert("This is an alert box");
 
 //Variables: it is a container that holds data in memory. It has a name and a value. We can use variables to store and manipulate data in our programs.
 //declarartion : let x;
 //assignment or initialization: x=5;
-let x=5;
+/*let x=5;
 console.log(x);
 //Numbers datadtype:
 let age=21;
@@ -100,12 +100,12 @@ console.log(`Bro is online: ${online}.`);
 
 let fullName="Harshit Mendiratta";
 let age1=21;
-let student=true;
-document.getElementById("p1").textContent=`My name is ${fullName}`;
-document.getElementById("p2").textContent=`You are ${age1} years old`;
-document.getElementById("p3").textContent=`Are you student:${student}`;
+let student=true;/*
+//document.getElementById("p1").textContent=`My name is ${fullName}`;
+//document.getElementById("p2").textContent=`You are ${age1} years old`;
+//document.getElementById("p3").textContent=`Are you student:${student}`;
 //arithmetic operator
-let a=10;
+/*let a=10;
 a=a+5;
 console.log(a);
 a=a-4;
@@ -115,16 +115,37 @@ console.log(a);
 a=a/2;
 console.log(a);
 //operator precedence : parenthesis, exponents, multiplication and divison and modulo , and addition and subtraction
-let result= 1+2*3+4**2; // 23
+let result= 1+2*3+4**2; // 23/*
 
 
 // accept some user input
-let username;
-username=window.prompt("Enter your name :");
-console.log(username);
 
 
-let num1=Number(window.prompt("Enter first number:"));
+
+/*let num1=Number(window.prompt("Enter first number:"));
 let num2=Number(window.prompt("Enter second number:"));
 let sum=num1+num2;
-console.log(`The sum of ${num1} and ${num2} is ${sum}.`);
+console.log(`The sum of ${num1} and ${num2} is ${sum}.`);*/
+
+/*let username;
+document.getElementById("mySubmit").onclick=function(){
+  username=document.getElementById("myText").value;
+  document.getElementById("myH1").textContent=`Hello ${username}`;
+}
+*/
+//type conversion:converting one type of data into another 
+/*let age=window.prompt("Enter your age :");
+age=Number(age);
+age+=1;
+console.log(age);*/
+
+let x="pizza";
+let y="pizza";
+let z="pizza";
+
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
