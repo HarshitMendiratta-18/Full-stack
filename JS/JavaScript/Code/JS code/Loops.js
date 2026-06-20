b2ffe87a36ce5a4c91f8ef04fefb211845bf8b3b
Harshit Mendiratta <harshitmendiratta41@gmail.com>
2026-06-20 22:04:12 +0530

    loops

--- JS/JavaScript/Code/JS code/Loops.js
+++ JS/JavaScript/Code/JS code/Loops.js
@@ -96,7 +96,16 @@ for(let i=1;i<=n;i++){
   }
   console.log(str);
 }
-
-
-
-
+//q6:
+let runs=100;
+for(let i=1;i<=runs;i++){
+  if(i%3===0 && i%5===0){
+    console.log("FizzBuzz");
+  }else if(i%3===0){
+    console.log("Fizz");
+  }else if(i%5===0){
+    console.log("Buzz");
+  }else{
+    console.log(i);
+  }
+}
