fa4ad6611fa904525c8fc1273ba92ec6f9f4da7b
Harshit Mendiratta <harshitmendiratta41@gmail.com>
2026-07-01 17:48:38 +0530

    Strings

---
+++ JS/JavaScript/Code/JS code/Array.js
@@ -0,0 +1,1 @@
+//array is list of items that can store multiple values in one place.
---
+++ JS/JavaScript/Code/JS code/String.js
@@ -0,0 +1,95 @@
+//string is a piece of text.
+// let name="Harshit";
+// let sentance=`My name is ${name}`;
+// console.log(sentance);
+
+// let str="harshit"+23;
+// console.log(str);
+// let beta="halwa"+str;
+// console.log(beta);
+
+// let firstname="Harshit";
+// let lastname="Mendiratta";
+// let fullname=`My name is ${firstname} ${lastname}`;
+// console.log(fullname);
+
+// let a=2;
+// let b=4;
+// let c=5;
+// let str=`Number 1 is: ${a} Number 2 is: ${b} Number 3 is: ${c}`;
+// console.log(str);
+
+//`` this is known as template literal and this method is knwon as string interpolation. It is used to combine variables and strings together.
+
+// let str=new String("Harshit");
+// console.log(str);
+
+// let str="hello \nhowareu"
+// console.log(str);
+// console.log(str.length);
+
+ //let str="hello world";
+// console.log(str[0]);
+// str[3]='u';
+// console.log(str)
+//strings are immutable in js. we cannot change the value of a string once it is created. we can only create a new string with the desired changes.
+
+// for(let ch of str){
+//   console.log(ch);
+// }
+
+// for(let key in str){
+//   console.log(key);
+// }
+
+//properties and methods:
+// let str="   Hello   ";
+// let strcopy=str.toUpperCase();
+// let trimmedstr=str.trim();
+// let strcopy2=str.toLowerCase();
+// console.log(trimmedstr);
+
+//  let str="I am harshit,and i am harshit";
+//  let check=str.includes("am");
+//  console.log(check);
+//  console.log(str.indexOf("h"));
+//  console.log(str.charAt(7));
+//  console.log(str.replace("harshit","Aman"));
+//  console.log(str.replaceAll("harshit","Aman"));
+
+// let str="@harshit";
+// console.log(str.slice(1,6));
+
+// let str="alpha beta gamma";
+// console.log(str.split("a"));
+
+//q1:
+// let fullname=prompt("Enter your fullname");
+// let username=`@${fullname.trim().replaceAll(" ","")}_${fullname.length}`;
+// console.log(username);
+
+//q2:
+// let str=prompt("Enter string");
+// let character=prompt("Enter a character");
+// let count=0;
+// for(let val of str){
+//   if(val.toLowerCase()===character.toLowerCase()){
+//     count++;
+//   }
+// for(let key in str){
+//   if(str[key].toLowerCase()===character.toLowerCase()){
+//     console.log("index:",key);
+//     count++;
+//   }
+// }
+// console.log("count:",count);
+
+//q3:
+// let str="i am a boy";
+// console.log(str.split(" ").length);
+
+
+
+
+
+
